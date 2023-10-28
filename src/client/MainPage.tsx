114f49f6e41fb68e436f4c6b932f3bbdc1ce2dc4
felipeog <17603069+felipeog@users.noreply.github.com>
2023-10-28 11:18:43 -0300

    Testing category's query and action

--- src/client/MainPage.tsx
+++ src/client/MainPage.tsx
@@ -1,13 +1,69 @@
+import { Category } from "@wasp/entities";
+import { FormEvent } from "react";
+import { useQuery } from "@wasp/queries";
 import { User } from "@wasp/entities";
+import createCategory from "@wasp/actions/createCategory";
+import getCategories from "@wasp/queries/getCategories";
 import logout from "@wasp/auth/logout";
-import "./Main.css";
+
+const CategoriesList = ({ categories }: { categories: Category[] }) => {
+  if (!categories?.length) {
+    return <div>No categories</div>;
+  }
+
+  return (
+    <ul>
+      {categories.map((category) => (
+        <li key={category.id}>{category.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+const NewCategoryForm = () => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const target = event.target as HTMLFormElement;
+      const name = target.categoryName.value;
+
+      await createCategory({ name });
+
+      target.reset();
+    } catch (err: any) {
+      window.alert("Error: " + err.message);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name="categoryName" type="text" defaultValue="" />
+      <input type="submit" value="Create category" />
+    </form>
+  );
+};
 
 const MainPage = ({ user }: { user: User }) => {
+  const { data: categories, isLoading, error } = useQuery(getCategories);
+
   return (
     <div className="container">
       <main>
-        <pre>{JSON.stringify({ user })}</pre>
-        <button onClick={logout}>Logout</button>
+        <div>
+          {categories && <CategoriesList categories={categories} />}
+          {isLoading && "Loading..."}
+          {error && "Error: " + error}
+        </div>
+
+        <div>
+          <NewCategoryForm />
+        </div>
+
+        <div>
+          <pre>{JSON.stringify({ user })}</pre>
+          <button onClick={logout}>Logout</button>
+        </div>
       </main>
     </div>
   );

8d75387d54fbe5205fae18c7fa092f60254945c3
felipeog <17603069+felipeog@users.noreply.github.com>
2023-11-01 00:20:05 -0300

    Add background fetching indicator

--- src/client/debug/components/Layout.tsx
+++ src/client/debug/components/Layout.tsx
@@ -1,6 +1,7 @@
 import { clsx } from "clsx";
 import { Link } from "@wasp/router";
 import { QueryContainer } from "./QueryContainer";
+import { useIsFetching } from "@tanstack/react-query";
 import { useLocation } from "react-router-dom";
 
 type TLayoutProps = {
@@ -33,36 +34,41 @@ const routes = [
 ];
 
 export function Layout(props: TLayoutProps) {
+  const isFetching = useIsFetching();
   const location = useLocation();
 
   const currentRoute = routes.find((route) => route.to === location.pathname);
 
   return (
-    <div className="container mx-auto px-4">
-      <nav className="border-b border-gray-300">
-        <ul className="flex gap-4">
-          {routes.map((route) => (
-            <li key={route.to}>
-              <Link
-                to={route.to}
-                className={clsx("block py-4", {
-                  underline: route.to === currentRoute?.to,
-                })}
-              >
-                {route.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+    <main className="container mx-auto px-4">
+      <header className="flex justify-between items-center border-b border-gray-300">
+        <nav>
+          <ul className="flex gap-4">
+            {routes.map((route) => (
+              <li key={route.to}>
+                <Link
+                  to={route.to}
+                  className={clsx("block py-4", {
+                    underline: route.to === currentRoute?.to,
+                  })}
+                >
+                  {route.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
 
-      <main className="mt-6">
+        <p>{Boolean(isFetching) ? "Fetching..." : "Idle"}</p>
+      </header>
+
+      <section className="mt-6">
         <h1 className="text-2xl mb-4">{currentRoute?.label}</h1>
 
         <QueryContainer isLoading={props.isLoading} error={props.error}>
           {props.children}
         </QueryContainer>
-      </main>
-    </div>
+      </section>
+    </main>
   );
 }

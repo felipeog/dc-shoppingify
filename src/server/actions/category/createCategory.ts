faa11f95d69bbec2aa3ce38edd8540eecad4e85f
felipeog <17603069+felipeog@users.noreply.github.com>
2023-11-01 00:09:37 -0300

    Add Zod

--- src/server/actions/category/createCategory.ts
+++ src/server/actions/category/createCategory.ts
@@ -1,6 +1,14 @@
 import { Category } from "@wasp/entities";
 import { CreateCategory } from "@wasp/actions/types";
+import { z, ZodError } from "zod";
 import HttpError from "@wasp/core/HttpError.js";
+
+const categorySchema = z.object({
+  name: z
+    .string()
+    .min(3, "The category name must contain at least 3 characters.")
+    .max(100, "The category name must contain at most 100 characters."),
+});
 
 export const createCategory: CreateCategory<
   Pick<Category, "name">,
@@ -8,6 +16,15 @@ export const createCategory: CreateCategory<
 > = async (args, context) => {
   if (!context.user) {
     throw new HttpError(401);
+  }
+
+  try {
+    categorySchema.parse(args);
+  } catch (error) {
+    const firstErrorMessage =
+      (error as ZodError).errors[0].message ?? "Invalid input.";
+
+    throw new HttpError(400, firstErrorMessage);
   }
 
   // TODO: prevent case insensitive duplicates

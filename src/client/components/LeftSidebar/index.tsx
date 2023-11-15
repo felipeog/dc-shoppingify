b295f25772732702b760cc2488c6b3fe4133f4df
felipeog <17603069+felipeog@users.noreply.github.com>
2023-11-15 19:26:14 -0300

    Style navbar items

--- src/client/components/LeftSidebar/index.tsx
+++ src/client/components/LeftSidebar/index.tsx
@@ -1,21 +1,35 @@
 import { clsx } from "clsx";
 import { ERightSidebar } from "../../types";
+import {
+  GrCart,
+  GrBarChart,
+  GrRotateLeft,
+  GrUnorderedList,
+  GrUser,
+} from "react-icons/gr";
 import { Link } from "@wasp/router";
+import { Tooltip } from "react-tooltip";
 import { useAppState } from "../../state";
 import { useLocation } from "react-router-dom";
 
 const routes = [
   {
+    id: "items",
     to: "/" as "/",
     label: "Items",
+    icon: <GrUnorderedList size="1.2rem" />,
   },
   {
+    id: "history",
     to: "/history" as "/history",
     label: "History",
+    icon: <GrRotateLeft size="1.2rem" />,
   },
   {
+    id: "statistics",
     to: "/statistics" as "/statistics",
     label: "Statistics",
+    icon: <GrBarChart size="1.2rem" />,
   },
 ];
 
@@ -35,27 +49,45 @@ export function LeftSidebar() {
 
   return (
     <section className="bg-white flex-shrink-0 flex flex-col justify-between w-24 overflow-x-hidden overflow-y-auto">
-      <Link to="/user">User</Link>
+      <Link to="/user">
+        <GrUser />
+      </Link>
 
       <nav>
-        <ul>
+        <ul className="flex flex-col gap-4">
           {routes.map((route) => (
-            <li key={route.to}>
+            <li
+              key={route.to}
+              className={clsx(
+                "relative flex justify-center items-center",
+                "before:absolute before:w-2 before:bg-orange-400 before:top-0 before:left-0 before:bottom-0 before:rounded-r-md",
+                {
+                  "before:block": route.to === currentRoute?.to,
+                  "before:hidden": route.to !== currentRoute?.to,
+                }
+              )}
+            >
               <Link
                 to={route.to}
-                className={clsx("block py-4", {
-                  underline: route.to === currentRoute?.to,
-                })}
+                className="block py-4 p-4"
+                data-tooltip-id={route.id}
+                data-tooltip-content={route.label}
               >
-                {route.label}
+                {route.icon}
               </Link>
             </li>
           ))}
         </ul>
       </nav>
 
+      {routes.map((route) => (
+        <Tooltip key={route.id} id={route.id} place="right" />
+      ))}
+
       {/* TODO: break into separate component */}
-      <button onClick={handleCartButtonClick}>Cart</button>
+      <button onClick={handleCartButtonClick}>
+        <GrCart />
+      </button>
     </section>
   );
 }

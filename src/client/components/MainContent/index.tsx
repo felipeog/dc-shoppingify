757f4ebf96e4c66b25a72335642f20a3cc5c8e9a
felipeog <17603069+felipeog@users.noreply.github.com>
2023-11-15 22:25:35 -0300

    Fix spacing

--- src/client/components/MainContent/index.tsx
+++ src/client/components/MainContent/index.tsx
@@ -5,7 +5,7 @@ type TMainContentProps = {
 export function MainContent(props: TMainContentProps) {
   return (
     <section className="bg-cyan-50 flex-grow overflow-x-hidden overflow-y-auto">
-      <div className="max-w-screen-xl mx-auto p-4">{props.children}</div>
+      <div className="max-w-screen-xl mx-auto p-7">{props.children}</div>
     </section>
   );
 }

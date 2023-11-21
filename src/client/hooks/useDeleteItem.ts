5bd73956b4d569548f9de2d44912d7012cf68dac
felipeog <17603069+felipeog@users.noreply.github.com>
2023-11-21 19:58:04 -0300

    Redirect on item deletion

--- src/client/hooks/useDeleteItem.ts
+++ src/client/hooks/useDeleteItem.ts
@@ -1,19 +1,22 @@
+import { ERightSidebar } from "../types";
 import { toast } from "react-toastify";
+import { useAppState } from "../state";
 import { useState } from "react";
 import deleteItemAction from "@wasp/actions/deleteItem";
 
 export function useDeleteItem() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<Error>();
+  const state = useAppState();
 
   async function deleteItem(itemId: number) {
     setIsLoading(true);
     setError(undefined);
 
     try {
-      await deleteItemAction({
-        id: itemId,
-      });
+      await deleteItemAction({ id: itemId });
+
+      state.selectedRightSidebar.value = ERightSidebar.ITEMS_LIST;
     } catch (error: any) {
       console.error(error);
       setError(error);
